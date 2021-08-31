a109d6f48d1304e8389671649d6bdb52de5ea578
caio lima de almeida <caio.lima337@hotmail.com>
2021-08-30 22:47:50 -0300

    Adicionar arquivos de projeto.

---
+++ Marvel/Marvel.Android/Render/ExtenderEntryRender.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Marvel.Renders;
+using Xamarin.Forms.Platform.Android;
+using Android.Content.Res;
+using Android.Graphics;
+using Marvel.Droid.Render;
+
+[assembly: ExportRenderer ( typeof ( ExtenderEntry ), typeof ( ExtenderEntryRender ) )]
+namespace Marvel.Droid.Render
+{
+    public class ExtenderEntryRender : EntryRenderer
+    {
+
+        public ExtenderEntryRender ( Context context ) : base ( context )
+        {
+
+        }
+
+        protected override void OnElementChanged ( ElementChangedEventArgs<Entry> e )
+        {
+
+            base.OnElementChanged ( e );
+
+            if (Control == null || e.NewElement == null) return;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Control.BackgroundTintList = ColorStateList.ValueOf ( Android.Graphics.Color.Transparent );
+            }
+            else
+            {
+                Control.Background.SetColorFilter ( Android.Graphics.Color.Transparent, PorterDuff.Mode.SrcAtop );
+
+            }
+        }
+    }
+}
---
+++ Marvel/Marvel/View/SplashScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Marvel.View
+{
+    public class SplashScreen : ContentPage
+    {
+        Label splashScreen;
+        public SplashScreen()
+        {
+
+            BackgroundColor = Color.FromHex ( "#E8121D" );
+
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var layout = new AbsoluteLayout();
+            splashScreen = new Label
+            {
+                Text = "MARVELOUS",
+                TextColor = Color.FromHex("fff"),
+                Style = (Style)Application.Current.Resources["BebasBoldLabelStyle"],
+                FontSize=64
+            };
+
+            AbsoluteLayout.SetLayoutFlags(splashScreen, AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashScreen, new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            layout.Children.Add(splashScreen);
+            Content = layout;
+        }
+
+        protected override async void OnAppearing()
+        {
+                base.OnAppearing();
+                await splashScreen.ScaleTo(1, 5000);
+                Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
