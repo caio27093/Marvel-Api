936ed055537c6cb8636876a8700fcd5bd49fbca6
caio lima de almeida <caio.lima337@hotmail.com>
2021-09-19 22:33:59 -0300

    Home page

--- Marvel/Marvel/View/MainPage.xaml.cs
+++ Marvel/Marvel/View/MainPage.xaml.cs
@@ -14,5 +14,80 @@ namespace Marvel
         {
             InitializeComponent ();
         }
+
+        List<string> Estados = new List<string>
+        {
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Acre",
+            "Alagoas",
+            "Amapá",
+            "Amazonas",
+            "Bahia",
+            "Ceará",
+            "Distrito Federal",
+            "Espírito Santo",
+            "Goiás",
+            "Maranhão",
+            "Mato Grosso",
+            "Mato Grosso do Sul",
+            "Minas Gerais",
+            "Pará",
+            "Paraíba",
+            "Paraná",
+            "Pernambuco",
+            "Piauí",
+            "Rio de Janeiro",
+            "Rio Grande do Norte",
+            "Rio Grande do Sul",
+            "Rondônia",
+            "Roraima",
+            "Santa Catarina",
+            "São Paulo",
+            "Sergipe",
+            "Tocantins"
+        };
+
+        private void txtBusca_TextChanged ( object sender, TextChangedEventArgs e )
+        {
+            if (txtBusca.Text.Length >= 3)
+            {
+                
+                var sugestao = Estados.Where ( c => c.ToLower ( ).Contains ( txtBusca.Text.ToLower ( ) ) );
+                listaHerois.ItemsSource = sugestao;
+                modoPadrao.IsVisible = false;
+                modoPesquisa.IsVisible = true;
+            }
+            else
+            {
+                modoPadrao.IsVisible = true;
+                modoPesquisa.IsVisible = false;
+            }
+        }
     }
 }
